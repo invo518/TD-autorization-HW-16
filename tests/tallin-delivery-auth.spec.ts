79c3c1f53ed96ee8a7ceced1022809feb2aebc4e
invo518 <innavodiana.84@gmail.com>
2026-05-09 19:52:18 +0300

    feat:added tests

--- tests/tallin-delivery-auth.spec.ts
+++ tests/tallin-delivery-auth.spec.ts
@@ -1,10 +1,16 @@
 import { test, expect } from '@playwright/test';
+import {faker} from "@faker-js/faker";
+import {configDotenv} from "dotenv";
 
-const TD_URL = 'https://fe-delivery.tallinn-learning.ee';
+configDotenv()
+const TD_URL = process.env.APP_URL;
+const randomUsername = faker.internet.username()
+const randomPassword = faker.internet.password()
+
 
 test.beforeEach(async ({ page }) => {
-    await page.goto(TD_URL);
-    await page.waitForLoadState('networkidle');
+    await page.goto(process.env.APP_URL);
+
 });
 
 test('TD negative auth test 1', async ({ page }) => {
@@ -13,9 +19,65 @@ test('TD negative auth test 1', async ({ page }) => {
     const signInBtn = page.locator('[data-name="signIn-button"]');
     const errorPopup = page.locator('[data-name="authorizationError-popup"]');
 
-
-    await username.fill('user3');
-    await password.fill('qwerqwerqwerqwerqwer');
+    await username.fill(randomUsername);
+    await password.fill(randomPassword);
+    await expect(signInBtn).toBeEnabled()
     await signInBtn.click();
     await expect(errorPopup).toBeVisible();
 });
+
+test('Positive auth test', async ({ page }) => {
+    const username = page.locator('#username');
+    const password = page.locator('[data-name="password-input"]');
+    const signInBtn = page.locator('[data-name="signIn-button"]');
+
+    await username.fill(process.env.username);
+    await password.fill(process.env.password);
+    await expect(signInBtn).toBeEnabled();
+    await signInBtn.click();
+    await expect(page.getByRole('heading', { name: 'Create Order' })).toBeVisible();
+});
+test('Switch language EN > RU', async ({ page }) => {
+
+    await page.goto(process.env.APP_URL);
+
+    const ruButton = page.locator('[data-name="language-ru"]');
+    const usernameInput = page.locator('#username');
+    const passwordInput = page.locator('[data-name="password-input"]');
+    const signInBtn = page.locator('[data-name="signIn-button"]');
+
+
+    await expect(ruButton).not.toHaveClass('language__button language__button_active');
+    await ruButton.click();
+    await expect(ruButton).toHaveClass("language__button language__button_active");
+    await expect(usernameInput).toHaveAttribute('placeholder', 'Логин');
+    await expect(passwordInput).toHaveAttribute('placeholder', 'Пароль');
+    await expect(signInBtn).toHaveText( "Войти");
+});
+test('Privacy Policy link is correct', async ({ page }) => {
+    await page.goto(process.env.APP_URL!);
+    const privacyPolicy = page.locator('[data-name="privacy-policy"]');
+
+    await expect(privacyPolicy).toBeVisible();
+    await expect(privacyPolicy).toBeEnabled();
+    await expect(privacyPolicy).toHaveAttribute('href', '/pdf/politics.pdf');
+});
+test('Cookie Policy link is correct', async ({ page }) => {
+    await page.goto(process.env.APP_URL!);
+    const cookiePolicy = page.locator('[data-name="cookie-policy"]');
+
+    await expect(cookiePolicy).toBeVisible();
+    await expect(cookiePolicy).toBeEnabled();
+    await expect(cookiePolicy).toHaveAttribute('href', '/pdf/cookie.pdf');
+});
+
+test('Terms of service link is correct', async ({ page }) => {
+    await page.goto(process.env.APP_URL!);
+    const termsServ = page.locator('[data-name="terms-of-service"]');
+
+    await expect(termsServ).toBeVisible();
+    await expect(termsServ).toBeEnabled();
+    await expect(termsServ).toHaveAttribute('href', '/pdf/conditions.pdf')
+});
+
+
